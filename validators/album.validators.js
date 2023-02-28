047cd53c3116e18538146b7c406870a8c02c28de
VZGLY <chaineux.gavin@gmail.com>
2023-02-28 14:46:23 +0100

    fix validators

--- validators/album.validators.js
+++ validators/album.validators.js
@@ -2,7 +2,7 @@ const yup = require('yup')
 
 const albumValidator = yup.object({
     title : yup.string().required().trim().max(50),
-    cover : yup.string().trim().optional()
+    cover : yup.string().trim().nullable()
 })
 
 module.exports = albumValidator
--- validators/artist.validators.js
+++ validators/artist.validators.js
@@ -2,9 +2,9 @@ const yup = require('yup')
 
 const artistValidator = yup.object({
     firstname : yup.string().required().trim(),
-	lastname : yup.string().trim().optional(),
-    birthdate : yup.date().optional().nullable(),
-	deathdate : yup.date().optional().nullable().min(yup.ref('birthdate'))
+	lastname : yup.string().trim().nullable(),
+    birthdate : yup.date().nullable(),
+	deathdate : yup.date().nullable().min(yup.ref('birthdate'))
 })
 
 module.exports = artistValidator
